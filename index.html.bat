74a0025a4db82325699ff7fd67ed964ebcd2a2b6
hch <47154972+jllcps@users.noreply.github.com>
2021-06-27 20:34:02 +0800

    new index page

---
+++ index.html.bat
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Navigation</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style type="text/css">
+        body {
+          background-color: #272822; 
+          font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+          color: #f8f8f2;
+        }
+
+        .vertical-menu {
+          width: 200px;
+          margin: auto;
+          width: 50%;
+          padding: 10px;
+          text-align: center;
+        }
+
+        .vertical-menu a {
+          background-color: #272822; 
+          color: #f8f8f2;
+          display: block;
+          padding: 15px;
+          font-size: larger;
+          text-decoration: none;
+        }
+
+        .vertical-menu a:hover {
+          background-color: #ccc;
+        }
+    </style>
+</head>
+<body>
+
+<div class="vertical-menu">
+  <h1>Navigation</h1>
+  <a href="text-editor/main.html">Text Editor</a>
+  <a href="epub-reader/main.html">EPUB Reader</a>
+</div>
+
+</body>
+</html>
