9d6860f515d2844a2694b10bd50f296e48963a71
jllcps <47154972+jllcps@users.noreply.github.com>
2022-02-05 03:49:53 +0800

    add contact method

--- index.html.bat
+++ index.html.bat
@@ -1,44 +1,119 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="en" class="h-100">
+
 <head>
-    <title>Navigation</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style type="text/css">
-        body {
-          background-color: #272822; 
-          font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-          color: #f8f8f2;
-        }
-
-        .vertical-menu {
-          width: 200px;
-          margin: auto;
-          width: 50%;
-          padding: 10px;
-          text-align: center;
-        }
-
-        .vertical-menu a {
-          background-color: #272822; 
-          color: #f8f8f2;
-          display: block;
-          padding: 15px;
-          font-size: larger;
-          text-decoration: none;
-        }
-
-        .vertical-menu a:hover {
-          background-color: #ccc;
-        }
-    </style>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>jllcps</title>
+  <script src="assets/bootstrap.min.js"></script>
+  <link href="assets/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/index.css" rel="stylesheet">
 </head>
-<body>
+  
+<body class="d-flex h-100">
+    
+  <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+    <header class="mb-auto text-center">
+      <div>
+        <h3 class="float-md-start mb-0">Home Page</h3>
+        <nav class="nav nav-masthead justify-content-center float-md-end">
+          <a class="nav-link" href="text-editor/main.html">Text Editor</a>
+          <a class="nav-link" href="epub-reader/main.html">EPUB Reader</a>
+        </nav>
+      </div>
+    </header>
 
-<div class="vertical-menu">
-  <h1>Navigation</h1>
-  <a href="text-editor/main.html">Text Editor</a>
-  <a href="epub-reader/main.html">EPUB Reader</a>
-</div>
+    <div class="accordion" id="accordionFlushExample">
+      <div class="accordion-item">
+        <h2 class="accordion-header" id="flush-headingZero">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseZero" aria-expanded="false" aria-controls="flush-collapseZero">
+            System Development
+          </button>
+        </h2>
+        <div id="flush-collapseZero" class="accordion-collapse collapse" aria-labelledby="flush-headingZero" data-bs-parent="#accordionFlushExample">
+          <div class="accordion-body"><ul>
+            <li>Backend: Django, Node.js</li>
+            <li>Frontend: basic HTML, CSS, React, AngularJS</li>
+            <li>Others: RESTful API, standard (JSON, XML), security (TLS, basic cryptography)</li>
+            <li>Teamwork/VCS: Agile (Scrum on Jira), Git</li>
+            <li><a href="https://gitfront.io/r/jllcps/7c9506994e450be64ca86b381a2a042e453196ce/HotZone/">Github</a> - <a href="https://jllcps-hotzone.herokuapp.com/">HotZone</a></li>
+            <ul>
+            <li>Role in team: backend (CRUD, clustering), data model, UI adjustment</li>
+            </ul>
+          </ul></div>
+        </div>
+      </div>
+      <div class="accordion-item">
+        <h2 class="accordion-header" id="flush-headingOne">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+            Machine Learning
+          </button>
+        </h2>
+        <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+          <div class="accordion-body"><ul>
+            <li>Library/Framework: NumPy, Pandas, Scikit-learn, Matplotlib, PyTorch</li>
+            <li><a href="https://gitfront.io/r/jllcps/3ef59ba95eadc6c32bc86ce9823cc7673f94b3c2/Machine-Learning/">Github</a></li>
+            <ul>
+              <li>Implementation: Logistic Regression, Random Forest</li>
+              <li>PyTorch: Convolutional Neural Network</li>
+            </ul>
+            <li>Algorithm/Technique: Adaline, SVM, KNN, K-means, DBSCAN, bagging, boosting</li>
+            <li>Others: data preprocessing, feature engineering, regularization, etc.</li>
+          </ul></div>
+        </div>
+      </div>
+      <div class="accordion-item">
+        <h2 class="accordion-header" id="flush-headingTwo">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+            Blockchain
+          </button>
+        </h2>
+        <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+          <div class="accordion-body"><ul>
+            <li><a href="https://gitfront.io/r/jllcps/ebd095c6b165b8015083bc649797172344107e1c/Blockchain/">Github</a></li>
+            <ul>
+              <li>Hyperledger Fabric: chaincode in Go, app in Node.js</li>
+              <li>Ethereum: smart contract in Solidity</li>
+          </ul>
+            <li>Knowledge in Security, Privacy, Consensus Model, etc.</li>
+          </ul></div>
+        </div>
+      </div>
+      <div class="accordion-item">
+        <h2 class="accordion-header" id="flush-headingThree">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+            Databases
+          </button>
+        </h2>
+        <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+          <div class="accordion-body"><ul>
+            <li>Conceptual Design (& schema): ERD, UML</li>
+            <li>RDBMS Normalization, Command Optimization</li>
+            <li>System: MySQL, PostgreSQL, MongoDB</li>
+            <li><a href="https://gitfront.io/r/jllcps/20425ca61f4309ed2c443b05a8ef0d7e274b49bf/Miscellaneous/tree/Library%20Management%20System%20%28DB%20design%29/">Github</a> - Library Management System</li>
+          </ul></div>
+        </div>
+      </div>
+      <div class="accordion-item">
+        <h2 class="accordion-header" id="flush-headingFour">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseFour">
+            Others
+          </button>
+        </h2>
+        <div id="flush-collapseFour" class="accordion-collapse collapse" aria-labelledby="flush-headingFour" data-bs-parent="#accordionFlushExample">
+          <div class="accordion-body"><ul>
+            <li>Mini-JavaScript Interpreter in Haskell (<a href="https://gitfront.io/r/jllcps/f563715f9a011965aa948dac62d55d2d431f20b5/Mini-JavaScript/">Github</a>)</li>
+            <li>Commodity Inventory System in C++ (<a href="https://gitfront.io/r/jllcps/87994d14e37418dbc41bceafd5e5745c74c284ba/Commodity-Inventory-System/">Github</a>)</li>
+            <li>Mini Game in Java (<a href="https://gitfront.io/r/jllcps/20425ca61f4309ed2c443b05a8ef0d7e274b49bf/Miscellaneous/tree/Java%20Mini%20Game/">Github</a>)</li>
+          </ul></div>
+        </div>
+      </div>
+    </div>
 
+    <main class="px-3 mt-auto text-center">
+      <p class="lead">An Undergraduate at HKU CS</p>
+    </main>
+
+  </div>
 </body>
 </html>
