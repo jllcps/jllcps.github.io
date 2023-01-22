d1005160c7471a4bdb25c8d51ff597b36ae96104
jllcps <47154972+jllcps@users.noreply.github.com>
2023-01-22 23:07:02 +0800

    test lazy load

--- assets/index.js
+++ assets/index.js
@@ -17,21 +17,15 @@ const skills = `
     - Collaborate: Agile (Scrum on Jira), Git
 `;
 
-const projects = `
-- Highlights
+const highlights = `
+- University
     - P2T-mod (FYP): trajectory prediction for autonomous driving ([link](https://gitfront.io/r/jllcps/RhncPKZCuwvu/FYP21037/), [webpage](https://wp.cs.hku.hk/2021/fyp21037/))
     - Image Super Resolution: extension to real-world degradation ([link](https://gitfront.io/r/jllcps/18938c156943fd060834db4a7c953819356d7ac8/HKU/tree/Machine%20Learning/Image%20Super%20Resolution/))
     - Language Model: methods include N-gram, LSTM, Transformer ([link](https://gitfront.io/r/jllcps/18938c156943fd060834db4a7c953819356d7ac8/HKU/tree/Machine%20Learning/Language%20Model/))
     - Distributed Transaction System: enforce consistency, atomicity with RAFT consensus ([link](https://gitfront.io/r/jllcps/18938c156943fd060834db4a7c953819356d7ac8/HKU/tree/Blockchain/Distributed%20Transaction%20System/))
     - Library Management System: DBMS design scheme & report ([link](https://gitfront.io/r/jllcps/18938c156943fd060834db4a7c953819356d7ac8/HKU/tree/Data%20Science/Library%20Management%20System/))
     - HotZone: pandemic tracking/logging system built with Django ([link](https://gitfront.io/r/jllcps/18938c156943fd060834db4a7c953819356d7ac8/HKU/tree/Applications/HotZone/), [webpage](https://jllcps-hotzone.herokuapp.com/))
-- Others
-    - Machine Learning: Sentiment Analysis ([link](https://gitfront.io/r/jllcps/18938c156943fd060834db4a7c953819356d7ac8/HKU/tree/Machine%20Learning/Sentiment%20Analysis%20with%20Grammar/)), Traditional Model ([link](https://gitfront.io/r/jllcps/18938c156943fd060834db4a7c953819356d7ac8/HKU/tree/Machine%20Learning/Traditional%20Model/))
-    - Data Science: Hadoop/Spark Processing ([link](https://gitfront.io/r/jllcps/18938c156943fd060834db4a7c953819356d7ac8/HKU/tree/Data%20Science/Hadoop%20&%20Spark%20Data%20Processing/)), Spatial Indexing ([link](https://gitfront.io/r/jllcps/18938c156943fd060834db4a7c953819356d7ac8/HKU/tree/Data%20Science/Spatial%20Index/))
-    - Blockchain: CRUD on Hyperledger Fabric ([link](https://gitfront.io/r/jllcps/18938c156943fd060834db4a7c953819356d7ac8/HKU/tree/Blockchain/Product%20Catalog%20on%20Hyperledger%20Fabric/)), Ethereum Solidity ([link](https://gitfront.io/r/jllcps/18938c156943fd060834db4a7c953819356d7ac8/HKU/tree/Blockchain/Solidity%20%28Ethereum%29%20codes/))
-    - AWS SDK: EC2 Monitor ([link](https://gitfront.io/r/jllcps/18938c156943fd060834db4a7c953819356d7ac8/HKU/tree/Applications/AWS%20SDK/EC2%20Monitor/)), Image Processor ([link](https://gitfront.io/r/jllcps/18938c156943fd060834db4a7c953819356d7ac8/HKU/tree/Applications/AWS%20SDK/Image%20Processor/))
-    - Applications: 24 Game ([link](https://gitfront.io/r/jllcps/18938c156943fd060834db4a7c953819356d7ac8/HKU/tree/Applications/24%20Game/)), Commodity Inventory System ([link](https://gitfront.io/r/jllcps/18938c156943fd060834db4a7c953819356d7ac8/HKU/tree/Applications/Commodity%20Inventory%20System/)), Mini-JavaScript ([link](https://gitfront.io/r/jllcps/18938c156943fd060834db4a7c953819356d7ac8/HKU/tree/Applications/Mini-JavaScript/))
-    - Miscellaneous: Java ([link](https://gitfront.io/r/jllcps/18938c156943fd060834db4a7c953819356d7ac8/HKU/tree/Miscellaneous/Java%20codes/)), C ([link](https://gitfront.io/r/jllcps/18938c156943fd060834db4a7c953819356d7ac8/HKU/tree/Miscellaneous/C%20codes/))
+- Internship
 `;
 
 const experience = `
@@ -46,6 +40,13 @@ const others = `
     - Text Editor ([link](https://jllcps.github.io/text-editor/main.html)): uses CodeMirror - [repo](https://github.com/codemirror/CodeMirror)
     - Diff Checker ([link](https://jllcps.github.io/diff-checker/main.html)): uses jsdiff - [repo](https://github.com/kpdecker/jsdiff)
     - EPUB Reader ([link](https://jllcps.github.io/epub-reader/main.html)): uses Epub.js - [repo](https://github.com/futurepress/epub.js/)
+- Projects
+    - Machine Learning: Sentiment Analysis ([link](https://gitfront.io/r/jllcps/18938c156943fd060834db4a7c953819356d7ac8/HKU/tree/Machine%20Learning/Sentiment%20Analysis%20with%20Grammar/)), Traditional Model ([link](https://gitfront.io/r/jllcps/18938c156943fd060834db4a7c953819356d7ac8/HKU/tree/Machine%20Learning/Traditional%20Model/))
+    - Data Science: Hadoop/Spark Processing ([link](https://gitfront.io/r/jllcps/18938c156943fd060834db4a7c953819356d7ac8/HKU/tree/Data%20Science/Hadoop%20&%20Spark%20Data%20Processing/)), Spatial Indexing ([link](https://gitfront.io/r/jllcps/18938c156943fd060834db4a7c953819356d7ac8/HKU/tree/Data%20Science/Spatial%20Index/))
+    - Blockchain: CRUD on Hyperledger Fabric ([link](https://gitfront.io/r/jllcps/18938c156943fd060834db4a7c953819356d7ac8/HKU/tree/Blockchain/Product%20Catalog%20on%20Hyperledger%20Fabric/)), Ethereum Solidity ([link](https://gitfront.io/r/jllcps/18938c156943fd060834db4a7c953819356d7ac8/HKU/tree/Blockchain/Solidity%20%28Ethereum%29%20codes/))
+    - AWS SDK: EC2 Monitor ([link](https://gitfront.io/r/jllcps/18938c156943fd060834db4a7c953819356d7ac8/HKU/tree/Applications/AWS%20SDK/EC2%20Monitor/)), Image Processor ([link](https://gitfront.io/r/jllcps/18938c156943fd060834db4a7c953819356d7ac8/HKU/tree/Applications/AWS%20SDK/Image%20Processor/))
+    - Applications: 24 Game ([link](https://gitfront.io/r/jllcps/18938c156943fd060834db4a7c953819356d7ac8/HKU/tree/Applications/24%20Game/)), Commodity Inventory System ([link](https://gitfront.io/r/jllcps/18938c156943fd060834db4a7c953819356d7ac8/HKU/tree/Applications/Commodity%20Inventory%20System/)), Mini-JavaScript ([link](https://gitfront.io/r/jllcps/18938c156943fd060834db4a7c953819356d7ac8/HKU/tree/Applications/Mini-JavaScript/))
+    - Miscellaneous: Java ([link](https://gitfront.io/r/jllcps/18938c156943fd060834db4a7c953819356d7ac8/HKU/tree/Miscellaneous/Java%20codes/)), C ([link](https://gitfront.io/r/jllcps/18938c156943fd060834db4a7c953819356d7ac8/HKU/tree/Miscellaneous/C%20codes/))
 `;
 
 var modal = document.getElementById("modal_body"),
@@ -66,8 +67,8 @@ function customSetContent() {
     document.getElementById('accordion-body-skills').innerHTML =
         marked.parse(skills);
 
-    document.getElementById('accordion-body-projects').innerHTML =
-        marked.parse(projects);
+    document.getElementById('accordion-body-highlights').innerHTML =
+        marked.parse(highlights);
 
     document.getElementById('accordion-body-experience').innerHTML =
         marked.parse(experience);
